879ec85d385dbab82765f0b1e13908c10859c231
andy.lv <andy.lv.vip@hotmail.com>
2019-04-01 00:02:22 +0800

    init project...

---
+++ src/main/java/org/mengyun/tcctransaction/server/TccTransactionServerApplication.java
@@ -0,0 +1,13 @@
+package org.mengyun.tcctransaction.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccTransactionServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TccTransactionServerApplication.class, args);
+    }
+
+}
